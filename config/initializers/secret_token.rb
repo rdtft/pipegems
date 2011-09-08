ee59c8099fa64695927f24c242dcf8cc6d54529e
Sasha Koss <kossnocorp@gmail.com>
2011-09-09 01:41:43 +0700

    Hello cruel world!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pipegems::Application.config.secret_token = '93ae66ee0fd30e7ed0696e6fb5e87285268dde26eb259a6ed4fe71f398340e2c5684e4ff2bbd860ce45bcc436096b7987e6df3dd635b609132cd3ab88ccea3a3'
