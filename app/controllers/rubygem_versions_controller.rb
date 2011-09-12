a8be72ec7da95ed0149010758d24ad54e074b2d9
Sasha Koss <kossnocorp@gmail.com>
2011-09-13 03:33:02 +0700

    Build nested resources on rubygem_versions new action

--- app/controllers/rubygem_versions_controller.rb
+++ app/controllers/rubygem_versions_controller.rb
@@ -2,4 +2,12 @@ class RubygemVersionsController < InheritedResources::Base
   belongs_to :rubygem
   actions :index, :show, :new, :create
   load_and_authorize_resource
+
+  def new
+    3.times do
+      @rubygem_version.rubygem_files.build
+    end
+    new!
+  end
+
 end
