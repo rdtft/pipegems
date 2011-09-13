3d2d7904cc92f029dec67116e293362e9c8fdcd6
Sasha Koss <kossnocorp@gmail.com>
2011-09-14 00:32:31 +0700

    Fix more typos in CarrierWave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ AWS_CONFIG = YAML.load_file("#{Rails.root}/config/aws.yml")[Rails.env]
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      AWS_CONFIG['access_key_id'],
-    aws_secret_access_key:  AWS_CONFIG['secret_access_key'],
+    aws_access_key_id:      AWS_CONFIG['aws_access_key_id'],
+    aws_secret_access_key:  AWS_CONFIG['aws_secret_access_key'],
     region:                 AWS_CONFIG['s3']['region']
   }
 
