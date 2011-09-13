a65feb8f5565d993d2259b7953cd9e9016de53cc
Sasha Koss <kossnocorp@gmail.com>
2011-09-14 00:14:51 +0700

    Configure CarrierWave to work with S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+AWS_CONFIG = YAML.load_file("#{Rails.root}/config/aws.yml")[Rails.env]
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:               'AWS',
+    aws_access_key_id:      AWS_CONFIG['access_key_id']
+    aws_secret_access_key:  AWS_CONFIG['secret_access_key']
+    region:                 AWS_CONFIG['s3']['region']
+  }
+
+  config.fog_directory  = AWS_CONFIG['s3']['dir']
+end
