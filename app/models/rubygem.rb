0410c4e5c01ad954d21d47ce10a74e83a3ffdeb7
Eduard Tsech <edtsech@gmail.com>
2011-09-16 10:22:32 +0200

    Added validation to Rubygem model

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -2,6 +2,8 @@ class Rubygem < ActiveRecord::Base
   belongs_to :user
   has_many :rubygem_versions
 
+  validates :name, :presence => true, :uniqueness => true
+
   def lib_name
     "pipe-#{name.downcase}"
   end
