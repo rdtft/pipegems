aa82785ad9d2532f1d831648feb287796a84444c
Sasha Koss <koss@nocorp.me>
2012-02-05 00:29:09 +0700

    Remove code related to serving gems (closes #9)

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Pipegems::Application.routes.draw do
     resources :rubygem_versions, :as => :versions
   end
 
-  resources :gems, only: [:index, :show], constraints: { id: /pipe-.+.gem/ }
-
   root :to => 'rubygems#index'
 end
