b7059ce55bd9b4b40083d7584104f771fee58b31
Sasha Koss <koss@nocorp.me>
2012-02-08 01:12:34 +0700

    Commit schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20110912191711) do
   create_table "gem_files", :force => true do |t|
     t.string   "file"
     t.integer  "rubygem_version_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
   add_index "gem_files", ["rubygem_version_id"], :name => "index_gem_files_on_rubygems_version_id"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(:version => 20110912191711) do
   create_table "rubygem_files", :force => true do |t|
     t.string   "file"
     t.integer  "rubygem_version_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
   add_index "rubygem_files", ["rubygem_version_id"], :name => "index_rubygem_files_on_rubygem_version_id"
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(:version => 20110912191711) do
   create_table "rubygem_versions", :force => true do |t|
     t.integer  "rubygem_id"
     t.string   "version"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "rubygem_versions", ["rubygem_id"], :name => "index_rubygem_versions_on_rubygem_id"
@@ -43,25 +43,25 @@ ActiveRecord::Schema.define(:version => 20110912191711) do
   create_table "rubygems", :force => true do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "rubygems", ["user_id"], :name => "index_rubygems_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "nickname"
     t.string   "github_token"
     t.string   "name"
